1ffecc3e55765ac591fbdf5df5733fbae87a1780
SBeltgens <sg.beltgens@student.avans.nl>
2025-03-27 12:43:18 +0100

    SimpleJSON

--- Assets/Scripts/APIservice.cs
+++ Assets/Scripts/APIservice.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using UnityEngine.Networking;
 using UnityEngine;
 using UnityEngine.UI;
-using Unity.AspNet.WebApi;
 using SimpleJSON;
 
 public class APIservice : MonoBehaviour
